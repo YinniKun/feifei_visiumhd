1380ef45c949e77e8a73be9b09df43444d1816c5
YinniKun <dongrichard123@gmail.com>
2025-08-12 15:09:45 -0400

    update files

---
+++ qc+segment.py
@@ -0,0 +1,339 @@
+'''
+Do QC (bin level and cell level) on VisiumHD samples
+Also save the QC results and segmentation results
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import anndata
+import geopandas as gpd
+import scanpy as sc
+
+from tifffile import imread, imwrite
+from csbdeep.utils import normalize
+from stardist.models import StarDist2D
+from shapely.geometry import Polygon, Point
+from scipy import sparse
+from matplotlib.colors import ListedColormap
+import argparse
+import os
+
+####### Helpers from 10X guide on binning
+def plot_mask_and_save_image(title, gdf, img, cmap, output_name=None, bbox=None):
+    if bbox is not None:
+        # Crop the image to the bounding box
+        cropped_img = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+    else:
+        cropped_img = img
+
+    # Plot options
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Plot the cropped image
+    axes[0].imshow(cropped_img, cmap='gray', origin='lower')
+    axes[0].set_title(title)
+    axes[0].axis('off')
+
+    # Create filtering polygon
+    if bbox is not None:
+        bbox_polygon = Polygon([(bbox[0], bbox[1]), (bbox[2], bbox[1]), 
+                                (bbox[2], bbox[3]), (bbox[0], bbox[3])])
+        # Filter for polygons in the box
+        intersects_bbox = gdf['geometry'].intersects(bbox_polygon)
+        filtered_gdf = gdf[intersects_bbox]
+    else:
+        filtered_gdf=gdf
+
+    # Plot the filtered polygons on the second axis
+    filtered_gdf.plot(cmap=cmap, ax=axes[1])
+    axes[1].axis('off')
+    axes[1].legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+
+    # Save the plot if output_name is provided
+    if output_name is not None:
+        plt.savefig(output_name, bbox_inches='tight')  # Use bbox_inches='tight' to include the legend
+    else:
+        plt.show()
+
+def plot_gene_and_save_image(title, gdf, gene, img, adata, bbox=None, output_name=None):
+
+    if bbox is not None:
+        # Crop the image to the bounding box
+        cropped_img = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+    else:
+        cropped_img = img
+
+    # Plot options
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Plot the cropped image
+    axes[0].imshow(cropped_img, cmap='gray', origin='lower')
+    axes[0].set_title(title)
+    axes[0].axis('off')
+
+    # Create filtering polygon
+    if bbox is not None:
+        bbox_polygon = Polygon([(bbox[0], bbox[1]), (bbox[2], bbox[1]), (bbox[2], bbox[3]), (bbox[0], bbox[3])])
+
+
+    # Find a gene of interest and merge with the geodataframe
+    gene_expression = adata[:, gene].to_df()
+    gene_expression['id'] = gene_expression.index
+    merged_gdf = gdf.merge(gene_expression, left_on='id', right_on='id')
+
+    if bbox is not None:
+        # Filter for polygons in the box
+        intersects_bbox = merged_gdf['geometry'].intersects(bbox_polygon)
+        filtered_gdf = merged_gdf[intersects_bbox]
+    else:
+        filtered_gdf = merged_gdf
+
+    # Plot the filtered polygons on the second axis
+    filtered_gdf.plot(column=gene, cmap='inferno', legend=True, ax=axes[1])
+    axes[1].set_title(gene)
+    axes[1].axis('off')
+    axes[1].legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+
+    # Save the plot if output_name is provided
+    if output_name is not None:
+        plt.savefig(output_name, bbox_inches='tight')  # Use bbox_inches='tight' to include the legend
+    else:
+        plt.show()
+
+def plot_clusters_and_save_image(title, gdf, img, adata, bbox=None, color_by_obs=None, output_name=None, color_list=None):
+    color_list=["#7f0000","#808000","#483d8b","#008000","#bc8f8f","#008b8b","#4682b4","#000080","#d2691e","#9acd32","#8fbc8f","#800080","#b03060","#ff4500","#ffa500","#ffff00","#00ff00","#8a2be2","#00ff7f","#dc143c","#00ffff","#0000ff","#ff00ff","#1e90ff","#f0e68c","#90ee90","#add8e6","#ff1493","#7b68ee","#ee82ee"]
+    if bbox is not None:
+        cropped_img = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+    else:
+        cropped_img = img
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    axes[0].imshow(cropped_img, cmap='gray', origin='lower')
+    axes[0].set_title(title)
+    axes[0].axis('off')
+
+    if bbox is not None:
+        bbox_polygon = Polygon([(bbox[0], bbox[1]), (bbox[2], bbox[1]), (bbox[2], bbox[3]), (bbox[0], bbox[3])])
+
+    unique_values = adata.obs[color_by_obs].astype('category').cat.categories
+    num_categories = len(unique_values)
+
+    if color_list is not None and len(color_list) >= num_categories:
+        custom_cmap = ListedColormap(color_list[:num_categories], name='custom_cmap')
+    else:
+        # Use default tab20 colors if color_list is insufficient
+        tab20_colors = plt.cm.tab20.colors[:num_categories]
+        custom_cmap = ListedColormap(tab20_colors, name='custom_tab20_cmap')
+
+    merged_gdf = gdf.merge(adata.obs[color_by_obs].astype('category'), left_on='id', right_index=True)
+
+    if bbox is not None:
+        intersects_bbox = merged_gdf['geometry'].intersects(bbox_polygon)
+        filtered_gdf = merged_gdf[intersects_bbox]
+    else:
+        filtered_gdf = merged_gdf
+
+    # Plot the filtered polygons on the second axis
+    plot = filtered_gdf.plot(column=color_by_obs, cmap=custom_cmap, ax=axes[1], legend=True)
+    axes[1].set_title(color_by_obs)
+    legend = axes[1].get_legend()
+    legend.set_bbox_to_anchor((1.05, 1))
+    axes[1].axis('off')
+
+    # Move legend outside the plot
+    plot.get_legend().set_bbox_to_anchor((1.25, 1))
+
+    if output_name is not None:
+        plt.savefig(output_name, bbox_inches='tight')
+    else:
+        plt.show()
+
+# Plotting function for nuclei area distribution
+def plot_nuclei_area(gdf,area_cut_off,path):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 4))
+    # Plot the histograms
+    axs[0].hist(gdf['area'], bins=50, edgecolor='black')
+    axs[0].set_title('Nuclei Area')
+
+    axs[1].hist(gdf[gdf['area'] < area_cut_off]['area'], bins=50, edgecolor='black')
+    axs[1].set_title('Nuclei Area Filtered:'+str(area_cut_off))
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, "bin_nuclei_area_distribution.png"))
+    plt.close()
+
+# Total UMI distribution plotting function
+def total_umi(adata_, cut_off, path):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+
+    # Box plot
+    axs[0].boxplot(adata_.obs["total_counts"], vert=False, widths=0.7, patch_artist=True, boxprops=dict(facecolor='skyblue'))
+    axs[0].set_title('Total Counts')
+
+    # Box plot after filtering
+    axs[1].boxplot(adata_.obs["total_counts"][adata_.obs["total_counts"] > cut_off], vert=False, widths=0.7, patch_artist=True, boxprops=dict(facecolor='skyblue'))
+    axs[1].set_title('Total Counts > ' + str(cut_off))
+
+    # Remove y-axis ticks and labels
+    for ax in axs:
+        ax.get_yaxis().set_visible(False)
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, "bin_total_umi_distribution.png"))
+    plt.close()
+
+######### MAIN Functions
+def qc(space_ranger, output) -> None:
+    # Load the SpaceRanger data
+    adata = sc.read(space_ranger)
+
+    # Perform initial QC
+    sc.pp.calculate_qc_metrics(adata, inplace=True)
+
+    # Visualize the QC metrics
+    sc.pl.violin(adata, ['n_genes', 'n_counts'], jitter=0.4)
+    plt.savefig(os.path.join(output, "bin_qc_metrics.png"))
+
+    # # Save the processed AnnData object
+    # adata.write(f"{output_file}_processed.h5ad")
+
+def segment(img: np.ndarray, dir_base: str, model_name: StarDist2D = "2D_versatile_he", 
+            normalize_img: bool = True) -> gpd.GeoDataFrame:
+
+    model = StarDist2D.from_pretrained(model_name)
+
+    if normalize_img:
+        img = normalize(img, pmin=5, pmax=95)
+
+    labels, polys = model.predict_instances_big(img, axes='YXC', 
+                                                block_size=4096, prob_thresh=0.01,nms_thresh=0.001, 
+                                                min_overlap=128, context=128, normalizer=None, n_tiles=(4,4,1))
+
+    # Creating a list to store Polygon geometries
+    geometries = []
+
+    # Iterating through each nuclei in the 'polys' DataFrame
+    for nuclei in range(len(polys['coord'])):
+
+        # Extracting coordinates for the current nuclei and converting them to (y, x) format
+        coords = [(y, x) for x, y in zip(polys['coord'][nuclei][0], polys['coord'][nuclei][1])]
+
+        # Creating a Polygon geometry from the coordinates
+        geometries.append(Polygon(coords))
+
+    # Creating a GeoDataFrame using the Polygon geometries
+    gdf = gpd.GeoDataFrame(geometry=geometries)
+    gdf['id'] = [f"ID_{i+1}" for i, _ in enumerate(gdf.index)]
+
+    # save plot for whole image
+    cmap=ListedColormap(['grey'])
+    plot_mask_and_save_image(title="Segmentation",gdf=gdf,bbox=None,
+                             cmap=cmap,img=img,
+                             output_name=dir_base+"segmentation.tif")
+    return gdf
+
+def binning(base_path: str, gdf: gpd.GeoDataFrame, output: str) -> None:
+    raw_h5_file = os.path.join(base_path, 'raw_feature_bc_matrix.h5')
+    tissue_position_file = os.path.join(base_path, "spatial", 'tissue_positions.parquet')
+    adata = sc.read_10x_h5(raw_h5_file)
+
+    # Load the Spatial Coordinates
+    df_tissue_positions=pd.read_parquet(tissue_position_file)
+
+    #Set the index of the dataframe to the barcodes
+    df_tissue_positions = df_tissue_positions.set_index('barcode')
+
+    # Create an index in the dataframe to check joins
+    df_tissue_positions['index']=df_tissue_positions.index
+
+    # Adding the tissue positions to the meta data
+    adata.obs =  pd.merge(adata.obs, df_tissue_positions, left_index=True, right_index=True)
+
+    # Create a GeoDataFrame from the DataFrame of coordinates
+    geometry = [Point(xy) for xy in zip(df_tissue_positions['pxl_col_in_fullres'], df_tissue_positions['pxl_row_in_fullres'])]
+    gdf_coordinates = gpd.GeoDataFrame(df_tissue_positions, geometry=geometry)
+    
+    # Perform a spatial join to check which coordinates are in a cell nucleus
+    result_spatial_join = gpd.sjoin(gdf_coordinates, gdf, how='left', predicate='within')
+
+    # Identify nuclei associated barcodes and find barcodes that are in more than one nucleus
+    result_spatial_join['is_within_polygon'] = ~result_spatial_join['index_right'].isna()
+    barcodes_in_overlaping_polygons = pd.unique(result_spatial_join[result_spatial_join.duplicated(subset=['index'])]['index'])
+    result_spatial_join['is_not_in_an_polygon_overlap'] = ~result_spatial_join['index'].isin(barcodes_in_overlaping_polygons)
+
+    # Remove barcodes in overlapping nuclei
+    barcodes_in_one_polygon = result_spatial_join[result_spatial_join['is_within_polygon'] & result_spatial_join['is_not_in_an_polygon_overlap']]
+
+    # The AnnData object is filtered to only contain the barcodes that are in non-overlapping polygon regions
+    filtered_obs_mask = adata.obs_names.isin(barcodes_in_one_polygon['index'])
+    filtered_adata = adata[filtered_obs_mask,:]
+
+    # Add the results of the point spatial join to the Anndata object
+    filtered_adata.obs =  pd.merge(filtered_adata.obs, barcodes_in_one_polygon[['index','geometry','id','is_within_polygon','is_not_in_an_polygon_overlap']], 
+                                   left_index=True, right_index=True)
+    
+    # Group the data by unique nucleous IDs
+    groupby_object = filtered_adata.obs.groupby(['id'], observed=True)
+
+    # Extract the gene expression counts from the AnnData object
+    counts = filtered_adata.X
+
+    # Obtain the number of unique nuclei and the number of genes in the expression data
+    N_groups = groupby_object.ngroups
+    N_genes = counts.shape[1]
+
+    # Initialize a sparse matrix to store the summed gene counts for each nucleus
+    summed_counts = sparse.lil_matrix((N_groups, N_genes))
+
+    # Lists to store the IDs of polygons and the current row index
+    polygon_id = []
+    row = 0
+
+    # Iterate over each unique polygon to calculate the sum of gene counts.
+    for polygons, idx_ in groupby_object.indices.items():
+        summed_counts[row] = counts[idx_].sum(0)
+        row += 1
+        polygon_id.append(polygons)
+
+    # Create and AnnData object from the summed count matrix
+    summed_counts = summed_counts.tocsr()
+    grouped_filtered_adata = anndata.AnnData(X=summed_counts,
+                                             obs=pd.DataFrame(polygon_id,columns=['id'],index=polygon_id),var=filtered_adata.var)
+
+    # Store the area of each nucleus in the GeoDataframe
+    gdf['area'] = gdf['geometry'].area
+
+    # Calculate quality control metrics for the original AnnData object
+    sc.pp.calculate_qc_metrics(grouped_filtered_adata, inplace=True)
+
+    # Plot the nuclei area distribution before and after filtering
+    plot_nuclei_area(gdf=gdf,area_cut_off=500, path=output)
+    total_umi(grouped_filtered_adata, 100, path=output)
+    # save grouped filtered adata
+    grouped_filtered_adata.write(os.path.join(output, "grouped_filtered_adata.h5ad"))
+    # return grouped_filtered_adata
+
+def main():
+    parser = argparse.ArgumentParser(description="Perform QC on VisiumHD samples")
+    parser.add_argument("--space_ranger", help="Path to the input SpaceRanger directory")
+    parser.add_argument("--tissue_img", help="Path to the input tissue image file")
+    parser.add_argument("--output", help="Path to the output file, summarizing QC metrics")
+    parser.add_argument("--exp_name", help="Name of the experiment")
+    args = parser.parse_args()
+    output_dir = os.path.join(args.output, args.exp_name)
+    os.makedirs(output_dir, exist_ok=True)
+    # run qc
+    qc(args.space_ranger, args.output)
+    # run segmentation
+    img = imread(args.tissue_img)
+    gdf = segment(img, output_dir, model_name="2D_versatile_he", normalize_img=True)
+    # binning
+    binning(args.space_ranger, gdf, output_dir)
+
+    print("Preliminary analysis complete.")
+
+
+if __name__ == "__main__":
+    main()
